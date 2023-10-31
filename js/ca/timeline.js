deafc1883b0faaddccd073a75bffcbfa8af80654
EloiSans <esansgis@gmail.com>
2023-10-31 17:14:13 +0100

    [NEW] multilanguages

--- js/ca/timeline.js
+++ js/ca/timeline.js
@@ -1,4 +1,4 @@
-import data from './json/en_data.json' assert { type: 'json' };
+import data from '../json/ca_data.json' assert { type: 'json' };
 
 console.log(data);
 window.timeline = new TL.Timeline('timeline-embed', data);
